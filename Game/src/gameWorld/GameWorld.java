c19d0584aafee77741dbe93eb9ae279ae5b5d5d5
mohamedabdelhakem1 <mohamedabdelhakem99@yahoo.com>
2018-12-18 01:06:49 +0200

    sa

--- Game/src/gameWorld/GameWorld.java
+++ Game/src/gameWorld/GameWorld.java
@@ -1,5 +1,6 @@
 package gameWorld;
 
+import java.awt.image.BufferedImage;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,8 +11,8 @@ import eg.edu.alexu.csd.oop.game.GameObject;
 import eg.edu.alexu.csd.oop.game.World;
 
 import gameObjects.Clown;
-
-
+import gameObjects.Plate;
+import gameObjects.PlateFactory;
 
 public class GameWorld implements World {
 	private int score = 0;
@@ -21,46 +22,51 @@ public class GameWorld implements World {
 	private final List<GameObject> constant = new LinkedList<GameObject>();
 	private final List<GameObject> moving = new LinkedList<GameObject>();
 	private final List<GameObject> control = new LinkedList<GameObject>();
-	
+	private PlateFactory factory;
+
 	public GameWorld(int height, int width) {
 		this.width = width;
 		this.height = height;
-
-
+		factory = PlateFactory.getInstance();
 		ClownEnginner clownEnginner = new ClownEnginner(100, 400, 20, 20);
 		clownEnginner.makeClown();
-		
 		control.add(clownEnginner.getClown());
-
+		for (int i = 0; i < 6; i++) {
+			moving.add(factory.getPlate(width,height));	
+		}	
 	}
-
+	
+	
+	@Override
+	public boolean refresh() {
+		for(GameObject plate : moving) {
+			plate.setY(plate.getY()+2);
+			plate.setX(plate.getX() + (Math.random() > 0.5 ? 1 : -1));
+			if(plate.getY()== height) {
+				plate.setY(0);
+			}
+		}
+		
+		return true;
+	}
 	@Override
 	public List<GameObject> getConstantObjects() {
-
-		// TODO Auto-generated method stub
 		return constant;
 
 	}
 
 	@Override
 	public List<GameObject> getMovableObjects() {
-
-		// TODO Auto-generated method stub
 		return moving;
-
 	}
 
 	@Override
 	public List<GameObject> getControlableObjects() {
-
-		// TODO Auto-generated method stub
 		return control;
-
 	}
 
 	@Override
 	public int getWidth() {
-		// TODO Auto-generated method stub
 		return width;
 	}
 
@@ -70,11 +76,7 @@ public class GameWorld implements World {
 		return height;
 	}
 
-	@Override
-	public boolean refresh() {
-		// TODO Auto-generated method stub
-		return true;
-	}
+
 
 	@Override
 	public String getStatus() {
@@ -84,8 +86,6 @@ public class GameWorld implements World {
 
 	@Override
 	public int getSpeed() {
-
-		
 		return 10;
 
 	}
